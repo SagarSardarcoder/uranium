de00e0bd212240a2ca7f5c803d37cf60c219732a
sagarsardar-uranium <sagarsardar.net@gmail.com>
2022-04-06 21:30:36 +0530

    this is the routes folder

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,31 +1,32 @@
 const express = require('express');
-const logger = require('./logger')
+const logger = require('../logger/logger')
+const delhi = require('../util/helper')
+const banglore = require('../validator/formatter')
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('I am inside the first route handler')
-    console.log('The endpoint value is', logger.endpoint)
-    console.log('Calling log function')
-    logger.logging()
-    res.send('My first ever api!')
+   logger.logging();
 });
 
 router.get('/test-me2', function (req, res) {
-    console.log('I am inside the second route handler')
-    res.send('My second ever api!')
+    delhi.exp1;
+    delhi.exp2;
+    delhi.exp3;
 });
 
-
-router.get('/test-me5', function (req, res) {
-    res.send('My final ever api!')
-});
 
 router.get('/test-me3', function (req, res) {
-    res.send('My first ever api!')
+   banglore.expo1;
+   banglore.expo2;
+   banglore.expo3;
 });
 
 router.get('/test-me4', function (req, res) {
+    res.send('My first ever api!')
+});
+
+router.get('/test-me5', function (req, res) {
     res.send('My first ever api!')
 });
 
