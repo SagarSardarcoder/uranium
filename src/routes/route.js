ad25cef21c4ac2b65cdfdda5c9b3f938c0818f06
sagarsardar-uranium <sagarsardar.net@gmail.com>
2022-04-07 23:09:05 +0530

    assignment for api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,66 @@
-// const express = require('express');
-// const logger = require('./logger')
-
-// const router = express.Router();
-
-router.get('/movies ', function (req, res) {
-    const movielist = ["justice league", "the shining", "lord of the rings", "bartman begins","avengers"]
-    const iterator = movielist.values();
-    res.send(iterator)
+const express = require('express');
+const router = express.Router();
+const movielist = ["justice league", "the shining", "lord of the rings", "bartman begins","avengers"]
+router.get('/movies', function (req, res) {
+    
+        const result = movielist.toString();
+          res.send(result) 
 });
+router.get('/movies/:indexNumber',function(req,res){
+    let nameOfMovie = ""
+    if(req.params.indexNumber > movielist.length){
+        nameOfMovie = "use a valid index"
+    }else{
+           nameOfMovie =  movielist[req.params.indexNumber]
+    }   
+    res.send(nameOfMovie)
+});
+
+router.get('/films',function(req,res){
+   const lists = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+        res.send(lists)
+});
+router.get('/films/:filmId',function(req,res){
+    const lists = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+   let nameOFFilm = "";
+    for(i = 0;i < lists.length;i++){
+        if(lists[i].id == req.params.filmId){
+            nameOFFilm = lists[i].name
+            break;
+        } 
+        else{
+            nameOFFilm = "No movie exists with this id"
+        }
+    }  
+    res.send(nameOFFilm)
+});
+
+
+
 
 
 
