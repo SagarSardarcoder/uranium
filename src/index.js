0d593b98226cb075294e40b122ffbfb3861eff2c
sagarsardar-uranium <sagarsardar.net@gmail.com>
2022-04-11 19:52:12 +0530

    practice

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
@@ -9,6 +10,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route);
+
+mongoose.connect("mongodb+srv://Sagar-functionup:radhaswami123@cluster0.7xlsi.mongodb.net/sagarsardar?retryWrites=true&w=majority",
+)
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
