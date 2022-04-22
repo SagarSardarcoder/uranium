8376bb2e434163f5618a1a12bba7b6dfe9b7dbb3
sagarsardar-uranium <sagarsardar.net@gmail.com>
2022-04-22 17:51:49 +0530

    assignment done

--- src/middleware/commonMiddleware.js
+++ src/middleware/commonMiddleware.js
@@ -1,21 +1,25 @@
 let jwt = require("jsonwebtoken");
 
 const middl = async function (req, res,next) {
+  try{
   let token = req.headers["x-Auth-token"];
   if (!token) token = req.headers["x-auth-token"];
   let userId = req.params.userId
 
-  if(!userId) return res.send({msg:"userId is mandatory field"})
+  if(!userId) return res.status(400).send({msg:"userId is mandatory field"})
 
   let user = await userModel.findById(userId);
-  if (!user) return res.send("No such user exists"); 
+  if (!user) return res.status(403).send("No such user exists"); 
   // console.log(userId)
   
   if (!token) return res.send({ status: false, msg: "token must be present" });
   let decodedToken = jwt.verify(token, "functionup-uranium")
   // console.log(decodedToken.userId)
-  if(!decodedToken) return res.send({ status: false, msg: "token is invalid"})
-  if(decodedToken.userId !== userId) return res.send({msg:"you are not autherised for this process"})
+  if(!decodedToken) return res.status(403).send({ status: false, msg: "token is invalid"})
+  if(decodedToken.userId !== userId) return res.status(403).send({msg:"you are not autherised for this process"})
+}catch(err){
+  res.status(500).send({error:err.message})
+}
 
 next()
  
